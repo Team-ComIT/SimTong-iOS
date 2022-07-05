45b7040eccb53235c5a8e7cda86f753546496095
baegteun <baegteun@gmail.com>
2022-07-05 23:21:54 +0900

    :tada: :: 프로젝트 Initialize

---
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -0,0 +1,11 @@
+import ProjectDescription
+
+public enum Environment {
+    public static let appName = "SimSimPulYi"
+    public static let targetName = "SimSimPulYi"
+    public static let targetTestName = "\(targetName)Tests"
+    public static let organizationName = "baegteun"
+    public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone, .ipad])
+    public static let platform = Platform.iOS
+    public static let baseSetting: SettingsDictionary = SettingsDictionary()
+}
