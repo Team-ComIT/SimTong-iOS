cd693cafe4a438d5bdd246845aeab47f179ea064
baegteun <baegteun@gmail.com>
2022-07-24 23:55:28 +0900

    :bento: :: Main Color Asset

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/Color/Colors.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension Color {
+    // MARK: - Main
+    static let main01 = Color("")
+}
+
+struct Colors_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("as")
+    }
+}
