aed25bab2b595ade1c9976c9c307f87b60ca7cc3
baegteun <baegteun@gmail.com>
2022-07-25 15:46:55 +0900

    :sparkles: :: STICon

--- Projects/App/Sources/Application/SimTongApp.swift
+++ Projects/App/Sources/Application/SimTongApp.swift
@@ -5,7 +5,7 @@ import DesignSystem
 struct SimTongApp: App {
     var body: some Scene {
         WindowGroup {
-            Colors_Previews.previews
+            Text("Hello World")
         }
     }
 }
--- Projects/UsertInterfaces/DesignSystem/Sources/Icon/Icons.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Icon/Icons.swift
@@ -7,8 +7,13 @@ fileprivate extension DesignSystemImages {
     }
 }
 
-struct STIcon: View {
-    enum Icon {
+public struct STIcon: View {
+    public init(_ icon: Icon, color: Color? = nil) {
+        self.icon = icon
+        self.color = color
+    }
+
+    public enum Icon: CaseIterable {
         case bell
         case bellBadge
         case person
@@ -31,13 +36,8 @@ struct STIcon: View {
     var icon: Icon
     var color: Color?
 
-    var body: some View {
-        makeImage()
-    }
-
-    func makeImage() -> some View {
+    public var body: some View {
         iconToImage()
-            .renderingMode(.template)
             .foregroundColor(color ?? .gray05)
             .frame(width: 28, height: 28)
     }
@@ -46,48 +46,70 @@ struct STIcon: View {
         switch icon {
         case .bell:
             return DesignSystemAsset.Icons.bell.suiImage
+                .renderingMode(.template)
         case .bellBadge:
             return DesignSystemAsset.Icons.bellBadge.suiImage
+                .renderingMode(.original)
         case .person:
             return DesignSystemAsset.Icons.person.suiImage
+                .renderingMode(.template)
         case .message:
             return DesignSystemAsset.Icons.message.suiImage
+                .renderingMode(.template)
         case .heart:
             return DesignSystemAsset.Icons.heart.suiImage
+                .renderingMode(.template)
         case .heartFill:
             return DesignSystemAsset.Icons.heartFill.suiImage
+                .renderingMode(.template)
         case .hand:
             return DesignSystemAsset.Icons.hand.suiImage
+                .renderingMode(.template)
         case .handFill:
             return DesignSystemAsset.Icons.handFill.suiImage
+                .renderingMode(.template)
         case .plus:
             return DesignSystemAsset.Icons.plus.suiImage
+                .renderingMode(.template)
         case .signOut:
             return DesignSystemAsset.Icons.signOut.suiImage
+                .renderingMode(.template)
         case .write:
             return DesignSystemAsset.Icons.write.suiImage
+                .renderingMode(.template)
         case .trash:
             return DesignSystemAsset.Icons.trash.suiImage
+                .renderingMode(.template)
         case .clip:
             return DesignSystemAsset.Icons.clip.suiImage
+                .renderingMode(.template)
         case .cartFill:
             return DesignSystemAsset.Icons.cartFill.suiImage
+                .renderingMode(.template)
         case .send:
             return DesignSystemAsset.Icons.send.suiImage
+                .renderingMode(.template)
         case .xmark:
             return DesignSystemAsset.Icons.xmark.suiImage
+                .renderingMode(.template)
         case .chevronDown:
             return DesignSystemAsset.Icons.chevronDown.suiImage
+                .renderingMode(.template)
         case .back:
             return DesignSystemAsset.Icons.chevronLeft.suiImage
+                .renderingMode(.template)
         }
     }
 }
 
-struct Icons_Previews: PreviewProvider {
-    static var previews: some View {
+public struct Icons_Previews: PreviewProvider {
+    public static var previews: some View {
         VStack {
-            STIcon(icon: .trash)
+            ForEach(STIcon.Icon.allCases, id: \.self) { icon in
+                STIcon(icon)
+            }
+            STIcon(.xmark, color: .extraBlack)
+            STIcon(.heartFill, color: .main05)
         }
     }
 }
