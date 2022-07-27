c446c44b2156c3b273cfb5a1b5f55e8d99813b9e
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-07-27 20:46:24 +0900

    :sparkles: TextField
    TextField

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/TextField/TextField.swift
@@ -0,0 +1,108 @@
+// swiftlint: disable all
+import SwiftUI
+import UIKit
+
+public struct CustomTextField: View {
+    @State var placeHolderText: String
+    var style: Style
+    var states: state
+    var buttonText: String?
+    var errorText: String?
+    var error: Bool?
+    var action: () -> Void
+    @State private var text: String = ""
+
+    public var body: some View {
+        VStack {
+            HStack {
+                TextField("", text: $text) {}
+                .modifier(TextFieldClearButton(text: $text))
+                .multilineTextAlignment(.leading)
+                .textFieldStyle(CustomTextFieldStyle(
+                    text: $text,
+                    placeholderText: $placeHolderText,
+                    style: style,
+                    state: states
+                )
+                )
+                if style == .button {
+                    Button(action: {}) {
+                        Text(buttonText ?? "")
+                            .stTypo(.m5, color: .white)
+                    }
+                    .frame(width: 72, height: 54)
+                    .background(states == .disabled ? Color.gray04 : Color.main05)
+                }
+            }
+            .background(states == .disabled ? Color.gray02
+                        : Color.gray01)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.main06, lineWidth: error ?? false ? 2 : 0)
+            )
+            .cornerRadius(5)
+            .padding(.horizontal)
+            if error == true {
+                HStack {
+                    Text(errorText ?? "")
+                        .padding(.leading, 24)
+                        .stTypo(.r7, color: Color.main06)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct TextFieldClearButton: ViewModifier {
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        HStack {
+            content
+            
+            if !text.isEmpty {
+                Button(
+                    action: { self.text = "" },
+                    label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(Color(UIColor.opaqueSeparator))
+                    }
+                )
+            }
+        }
+    }
+}
+
+public struct PlaceholderStyle: ViewModifier {
+    var showPlaceHolder: Bool
+    var placeholder: String
+    var foregroundColor: Color
+
+    public func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            if showPlaceHolder {
+                Text(placeholder)
+                    .stTypo(.r5, color: foregroundColor)
+                    .padding()
+            }
+            content
+                .padding()
+        }
+    }
+}
+
+public struct TextField_Previews: PreviewProvider {
+    public static var previews: some View {
+        VStack(spacing: 30) {
+            Spacer()
+            CustomTextField(placeHolderText: "Input Text", style: .default, states: .disabled, buttonText: "text") {}
+            CustomTextField(placeHolderText: "Input Text", style: .default, states: .enabled, buttonText: "text") {}
+            CustomTextField(placeHolderText: "Input Text", style: .button, states: .disabled, buttonText: "text") {}
+            CustomTextField(placeHolderText: "Input Text", style: .button, states: .enabled, buttonText: "text") {}
+            CustomTextField(placeHolderText: "Input Text", style: .button, states: .enabled, buttonText: "text", errorText: "errorText", error: true) {}
+            Spacer()
+        }
+        .background(Color.gray03)
+    }
+}
---
+++ Projects/UsertInterfaces/DesignSystem/Sources/TextField/TextFieldStyle.swift
@@ -0,0 +1,38 @@
+// swiftlint: disable all
+import SwiftUI
+import UIKit
+
+extension CustomTextField {
+    enum Style {
+        case `default`, button
+    }
+    enum state {
+        case disabled, enabled
+    }
+}
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    
+    @Binding var text: String
+    @Binding var placeholderText: String
+    
+    var style: CustomTextField.Style
+    var state: CustomTextField.state
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        switch state {
+        case .disabled:
+            return ZStack {
+                configuration
+                    .modifier(PlaceholderStyle(showPlaceHolder: text.isEmpty, placeholder: placeholderText, foregroundColor: .gray04))
+                    .background(Color.gray02)
+            }
+        case .enabled:
+            return ZStack {
+                configuration
+                    .modifier(PlaceholderStyle(showPlaceHolder: text.isEmpty, placeholder: placeholderText, foregroundColor: .gray09))
+                    .background(Color.gray01)
+            }
+        }
+    }
+}
