d2ad85321d9b491a49f4985aaa8afe4d8c89716c
baegteun <74440939+baekteun@users.noreply.github.com>
2022-08-03 01:54:26 +0900

    Update Projects/App/Sources/Application/SimTongApp.swift

--- Projects/App/Sources/Application/SimTongApp.swift
+++ Projects/App/Sources/Application/SimTongApp.swift
@@ -5,7 +5,7 @@ import DesignSystem
 struct SimTongApp: App {
     var body: some Scene {
         WindowGroup {
-            TextField_Previews.previews
+            Text("Hello World")
         }
     }
 }
