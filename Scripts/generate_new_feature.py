b5f580e1b265a1e2c1e2d8438c88fec4c9b0c5d3
baegteun <baegteun@gmail.com>
2022-08-08 12:55:39 +0900

    :memo: :: generate_new_feature

--- Scripts/generate_new_feature.py
+++ Scripts/generate_new_feature.py
@@ -34,11 +34,11 @@ import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
     name: "{feature_name}Feature",
-    productt: .staticFramework,
+    product: .staticFramework,
     dependencies: [
         .Project.Features.CommonFeature"""
     file_content += ",\n        ".join(dependencies)
-    file_content += "\n    ]" 
+    file_content += "\n    ]"
     file_content += ",\n    hasDemo: true" if has_demo else ""
     file_content += "\n)"
     write_code_in_file(project_path, file_content)
@@ -72,7 +72,33 @@ class TargetTests: XCTestCase {
 '''
     write_code_in_file(test_file_path, test_content)
 
+def write_created_feature_on_dependency_project(feature_name):
+    file_path = f'{root_path}/Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+Project.swift'
+    read_file = read_file_at(file_path)
+    updated_file = update_file_at(read_file, feature_name)
+    write_file_at(file_path, updated_file)
 
+def read_file_at(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+def update_file_at(dependency_file, feature_name):
+    dependency_file_len = len(dependency_file)
+    for index, elem in enumerate(dependency_file):
+        if "public extension TargetDependency.Project.Features" in elem:
+            insert_line = index + 1
+            break
+        
+    append_code = f'    static let {feature_name}Feature = TargetDependency.feature(name: "{feature_name}Feature")\n'
+
+    if dependency_file_len > int(insert_line):
+        dependency_file.insert(insert_line, append_code)
+    
+    return dependency_file
+
+def write_file_at(file_path, update_file):
+    with open(file_path, 'w') as file:
+        file.writelines(update_file)
 
 print('Input new feature name ', end=': ', flush=True)
 feature_name = sys.stdin.readline().replace("\n", "")
@@ -89,3 +115,4 @@ root_path = os.getcwd()
 os.chdir(root_path + '/Projects/Features')
 
 make_new_feature(feature_name, has_demo)
+write_created_feature_on_dependency_project(feature_name)
