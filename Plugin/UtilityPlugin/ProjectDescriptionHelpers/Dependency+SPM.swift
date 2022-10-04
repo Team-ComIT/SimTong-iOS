2271bd75408a1ea66b2fdf82746a3a0ab764a415
baegteun <baegteun@gmail.com>
2022-10-05 08:07:02 +0900

    :heavy_plus_sign: :: Moya

--- Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -8,4 +8,5 @@ public extension TargetDependency {
 public extension TargetDependency.SPM {
     static let Lottie = TargetDependency.external(name: "Lottie")
     static let Needle = TargetDependency.external(name: "NeedleFoundation")
+    static let Moya = TargetDependency.external(name: "Moya")
 }
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,6 +4,7 @@ import ProjectDescriptionHelpers
 let dependencies = Dependencies(
     swiftPackageManager: [
         .remote(url: "https://github.com/airbnb/lottie-ios.git", requirement: .upToNextMajor(from: "3.4.3")),
-        .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.19.0"))
+        .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.19.0")),
+        .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.3"))
     ]
 )
