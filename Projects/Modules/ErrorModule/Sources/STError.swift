a15c8467542217253c341f8100924bed36a605a8
baegteun <baegteun@gmail.com>
2022-10-12 17:54:29 +0900

    :sparkles: :: UsersAPI Endpoint Listing

--- Projects/Modules/ErrorModule/Sources/STError.swift
+++ Projects/Modules/ErrorModule/Sources/STError.swift
@@ -5,9 +5,20 @@ public enum STError: Error {
 
     // global
     case internalServerError
+    case accessTokenExpired
 
     // common
     case notFoundUserByFindEmployeeNumber
+
+    // users
+    case notFoundUserBySignin
+    case passwordMismatch
+    case emailIsNotAuthorized
+    case userInfoIsAlreadyExistBySignup
+    case notExistsUserByVerifyEmployee
+    case passwordMismatchByChangePassword
+    case alreadyExistNicknameByChangeNickname
+    case alreadyExistEmailByChangeEmail
 }
 
 extension STError: LocalizedError {
@@ -21,6 +32,33 @@ extension STError: LocalizedError {
 
         case .internalServerError:
             return "서버에서 문제가 발생했습니다"
+
+        case .accessTokenExpired:
+            return "세션이 만료되었습니다. 다시 로그인해주세요!"
+
+        case .notFoundUserBySignin:
+            return "사원번호에 따른 유저를 찾을 수 없습니다"
+
+        case .passwordMismatch:
+            return "비밀번호가 일치하지 않습니다"
+
+        case .emailIsNotAuthorized:
+            return "이메일이 인증되지 않았습니다"
+
+        case .userInfoIsAlreadyExistBySignup:
+            return "이메일 또는 사원번호, 또는 닉네임이 이미 사용중입니다"
+
+        case .notExistsUserByVerifyEmployee:
+            return "일치하는 정보를 찾을 수 없습니다"
+
+        case .passwordMismatchByChangePassword:
+            return "비밀번호가 일치하지 않습니다"
+
+        case .alreadyExistNicknameByChangeNickname:
+            return "닉네임이 이미 사용중입니다"
+
+        case .alreadyExistEmailByChangeEmail:
+            return "이메일이 이미 사용중입니다"
         }
     }
 }
---
+++ Projects/Services/DataMappingModule/Sources/Users/Response/UserInfoResponseDTO.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public struct UserInfoResponseDTO: Decodable {
+    public init(
+        name: String,
+        email: String,
+        nickname: String,
+        spot: String,
+        profileImagePath: String
+    ) {
+        self.name = name
+        self.email = email
+        self.nickname = nickname
+        self.spot = spot
+        self.profileImagePath = profileImagePath
+    }
+
+    public let name: String
+    public let email: String
+    public let nickname: String
+    public let spot: String
+    public let profileImagePath: String
+
+    enum CodingKeys: String, CodingKey {
+        case name, email, nickname, spot
+        case profileImagePath = "profile_image_path"
+    }
+}
