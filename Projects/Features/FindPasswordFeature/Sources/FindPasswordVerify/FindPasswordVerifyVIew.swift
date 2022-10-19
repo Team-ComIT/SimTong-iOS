9120cdb300b6a989e306825ca11feaeaf354a4f0
baegteun <baegteun@gmail.com>
2022-10-19 20:39:31 +0900

    :sparkles: ::

--- Projects/Features/FindPasswordFeature/Sources/FindPasswordVerify/FindPasswordVerifyVIew.swift
+++ Projects/Features/FindPasswordFeature/Sources/FindPasswordVerify/FindPasswordVerifyVIew.swift
@@ -6,16 +6,10 @@ struct FindPasswordVerifyVIew: View {
     public init(
         viewModel: FindPasswordVerifyViewModel
     ) {
-        self.viewModel = viewModel
+        _viewModel = viewModel
     }
 
     var body: some View {
         Text("Hello, World!")
-    }
-}
-
-struct FindPasswordVerifyVIew_Previews: PreviewProvider {
-    static var previews: some View {
-        FindPasswordVerifyVIew()
     }
 }
