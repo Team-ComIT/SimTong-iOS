7bc469969aa8c01ba3fb940ebad1080ce4268fe8
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-12-20 20:17:54 +0900

    :fire: Lottie
    Lottie

--- Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -6,7 +6,6 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.SPM {
-    static let Lottie = TargetDependency.external(name: "Lottie")
     static let Needle = TargetDependency.external(name: "NeedleFoundation")
     static let Moya = TargetDependency.external(name: "Moya")
     static let Quick = TargetDependency.external(name: "Quick")
