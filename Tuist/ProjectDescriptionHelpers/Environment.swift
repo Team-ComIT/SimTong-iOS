d34c271da28b0793682fbfa505d5747c4ba56fb4
baegteun <baegteun@gmail.com>
2023-01-02 16:51:19 +0900

    :art: :: iPad 지원 대상 제거

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -5,7 +5,7 @@ public enum Environment {
     public static let targetName = "Simtong"
     public static let targetTestName = "\(targetName)Tests"
     public static let organizationName = "com.sungsimdang"
-    public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone, .ipad])
+    public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone])
     public static let platform = Platform.iOS
     public static let baseSetting: SettingsDictionary = ["OTHER_LDFLAGS": "$(inherited) -all_load"]
 }
