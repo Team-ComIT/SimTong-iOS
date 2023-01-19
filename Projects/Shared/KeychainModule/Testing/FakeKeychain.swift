4a8b2c09827e7f74603dab5bd7eec296e0b6c7b7
baegteun <baegteun@gmail.com>
2023-01-19 13:56:33 +0900

    :sparkles: :: Keychain Implementation

---
+++ Projects/Shared/KeychainModule/Testing/FakeKeychain.swift
@@ -0,0 +1,18 @@
+import Foundation
+import KeychainModuleInterface
+
+final class FakeKeyChain: Keychain {
+    var store: [String: String] = [:]
+
+    func save(type: KeychainType, value: String) {
+        store[type.rawValue] = value
+    }
+
+    func load(type: KeychainType) -> String {
+        store[type.rawValue] ?? ""
+    }
+
+    func delete(type: KeychainType) {
+        store[type.rawValue] = nil
+    }
+}
